11ba1e34497d3f95eb7e330d5c2d6fc60831b614
jayalakshmi601 <152255852+jayalakshmi601@users.noreply.github.com>
2025-07-15 13:43:39 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,5 +1,5 @@
 
-resource "aws-iam_user_policy_attachment" "namitha"{
+resource "aws-iam_user_policy_attachment" "jaya"{
 user=[aws_iam_user.jaya_4.name]
 policy_arn ="arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
