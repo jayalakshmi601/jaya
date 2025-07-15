e220fcf7eae0351e620e3615392e0718e00e0569
jayalakshmi601 <152255852+jayalakshmi601@users.noreply.github.com>
2025-07-15 13:44:17 +0530

    Update userattachment.tf

--- userattachment.tf
+++ userattachment.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_group_membership" "namitha" {
+resource "aws_iam_group_membership" "jaya" {
   name = "jaya_4"
 users =[aws_iam_user.jaya_4.name]
-group = aws_iam_group.namitha.name
+group = aws_iam_group.jaya.name
 }
